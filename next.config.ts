78326d1bf5c632c53e251909743390017806f956
Hubert Karwowski <hubert.karwowski01@gmail.com>
2024-10-25 14:39:16 +0200

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  experimental: {
+    taint: true,
+  },
+  // ...other config settings
 };
 
 export default nextConfig;
